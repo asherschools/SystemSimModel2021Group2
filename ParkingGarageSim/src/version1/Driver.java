2c4218e18bdb4c49a6f5d497cb3358a1e25ef8fa
digiovann9 <38504090+digiovann9@users.noreply.github.com>
2021-04-17 18:33:10 -0400

    Added a thing

--- ParkingGarageSim/src/version1/Driver.java
+++ ParkingGarageSim/src/version1/Driver.java
@@ -1,10 +1,26 @@
 package version1;
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime; 
 
 public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		boolean run = true;
+		while(run) {
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+			LocalDateTime now = LocalDateTime.now();  
+			System.out.println(dtf.format(now));  
+			
+			try {
+				
+				Thread.sleep(4000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 }
